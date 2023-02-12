e339d7341205ac90b7a1396bd2e90c655eda23e4
phil <jaeja.phil@gmail.com>
2023-02-12 10:23:06 +0900

    initial commit

---
+++ src/test/java/kr/api/lenders/LendersApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.api.lenders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
